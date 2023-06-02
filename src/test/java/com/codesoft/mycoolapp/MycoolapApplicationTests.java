6cc4a39ef53ca6999efc4ea67555e48fd743230c
Jakubster2 <jakub.kulesza022@gmail.com>
2023-06-02 15:58:15 +0200

    Add pom file

---
+++ src/test/java/com/codesoft/mycoolapp/MycoolapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesoft.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
